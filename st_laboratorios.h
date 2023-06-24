b4b7a08a91a0615825200ea4fa8a20cb7b037ca5
ManGab16 <136511243+ManGab16@users.noreply.github.com>
2023-06-24 20:42:00 -0300

    Update st_laboratorios.h
    alta, id autoincremental, baja

--- st_laboratorios.h
+++ st_laboratorios.h
@@ -1,17 +1,25 @@
 #ifndef LABORATORIOS_H_INCLUDED
 #define LABORATORIOS_H_INCLUDED
 
- typedef struct
- {
-     int idLab;
-     int idPaciente;
-     int anio;
-     int mes;
-     int dia;
-     int idPractica;
-     int baja;
+///-----Prototipado----------
 
- }StLaboratorios;
+typedef struct
+{
+    int idLab;
+    int idPaciente;         /// campo comun a pacientes.
+    int anio;               /// ------------------------------------------------------>
+    int mes;                /// 1 a 12.----------------------------------------------->  FECHA DEL ESTUDIO
+    int dia;                /// 1 a depende mes.-------------------------------------->
+    int practicaRealizada;  /// campo comun a practicas.(1 a 10)
+    int baja;               /// 0 o -1.
+}stLaboratorio;
 
+void altaLaboratorios(char archivo[]);
+//void fMes(int mes);
+//void fDia(int dia);
+
+int ultimoIDlab(char archivo[]);
+
+void bajaLaboratorios(int dia, int mes, int anio);
 
 #endif // LABORATORIOS_H_INCLUDED
